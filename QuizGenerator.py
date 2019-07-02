241d64eb1cfab44d9f8f89897de39b1dd1d470dc
Alex Ambrioso <aambrioso1@gmail.com>
2019-07-02 11:17:52 -0400

    Update QuizGenerator.py

--- QuizGenerator.py
+++ QuizGenerator.py
@@ -1,7 +1,7 @@
 """
-A quiz generator by Alex Ambrioso (2019
-)
-This is my reworkimg of a clever program found in the nice online etext "Automate the Boring Stuff"  by Al Sweigart.  The program creates quizzes with questions and answers in random order, along with the answer key.  The program in the book created quizzes on states amd their capitals.  I have generalized it so that it creates quizzes on any reasonable list of matched items.  To use my version you will need to create a text file called quizpairs.txt in the working directory.  This file should contain all the matching terms for the quizzes.  Every two lines of the file should contain a matched pair.   For example, if you would like to create quizzes on the states and their capitals, the file might start as follows:
+A quiz generator by Alex Ambrioso (2019)
+
+This is my reworking of a clever program found in the nice online etext "Automate the Boring Stuff"  by Al Sweigart.  The program creates quizzes with questions and answers in random order, along with the answer key.  TIn the book he program was used create quizzes on states and their capitals.  I have generalized it so that it creates quizzes on any reasonable list of matched items.  To use my version you will need to create a text file called quizpairs.txt in the working directory.  This file should contain all the matching terms for the quizzes.  Every two lines of the file should contain a matched pair.   For example, if you would like to create quizzes on the states and their capitals, the file might start as follows:
 
 Florida
 Tallahassee
@@ -10,12 +10,12 @@ Atlanta
 New York
 Albany
 
-Create at least ten matched pairs for your file.
-Omce you create this file in the appropriate directoey you can run the program.  It will generate 5 random quizzes based on your matching pairs.  These quizzes amd their answer keys will be saved as text files in your working directory.  You can create more or fewer quizzes by adjusting the variable num.
+You should create at least ten matched pairs for your file.
+Once you create this file in the appropriate directory, the working directory for your Python installation, you can run the program.  It will generate 5 random quizzes based on your matching pairs.  These quizzes and their answer keys will be saved as text files in your working directory.  You can create more or fewer quizzes by adjusting the variable num.
 """
 
 """
-I thought it my be fun and practical to create quizzes on Python terms.  Here is a list of Python terms amd expressions that could be used to make such quizzes.   Cut and paste this list, or create your own,  to the text file.  Technically each lime should end in a mewline.  I am not sure if cut and paste will do this so keep that in mind if you use my list below.
+I thought it my be fun and practical to create quizzes on Python terms.  Here is a list of Python terms amd expressions that could be used to make such quizzes.   Cut and paste this list to the text file or create your own list.  Technically each line should end in a newline.  I am not sure if cut and paste will do this so keep that in mind if you use my list below.
 
 list
 [1,'a',7]
@@ -39,25 +39,26 @@ exponentiation
 **
 
 """
-# We use the random lobrary to shuffle the questions.
+# We use the random library to shuffle the questions.
 import random
 
 # We use the file quizpairs.txt to create a dictionary of matching items for the quizzes.
 f1 = open('quizpairs.txt')
-l1=[]
-l2=[]
+qlist=[] # A list used to store the first item of the quiz pairs.
+alist=[] # A list used to store the second item of the quiz pairs.
 pos = 0
 for line in f1:
-    if pos % 2 == 0: l1.append(line.rstrip())
+    if pos % 2 == 0: qlist.append(line.rstrip())
     else: l2.append(line.rstrip())
     pos += 1
-terms=dict(zip(l1,l2))
+terms=dict(zip(qlist,alist))
 
+#  You should always close open files so that any resouces used by the file a freed up.
 f1.close()
 
-# Generate num quiz files and answer keys.
-num = 5
 
+num = 5 #  The number of quizzes to be generated.
+#  Generates the quiz files and answer keys.
 for quizNum in range(num):
     # Create the quiz and answer key files.
     quizFile = open('termquiz%s.txt' % (quizNum + 1), 'w')
@@ -82,7 +83,7 @@ for quizNum in range(num):
         answerOptions = wrongAnswers + [correctAnswer]
         random.shuffle(answerOptions)
         
-        # Write the question and the answer options to the quiz file.
+        # Write the question and the answer options to the quiz file.   The next line shows the format of the quiz questions.
         quizFile.write('%s. Which of the follwing best matches with %s?\n' % (questionNum + 1, questions[questionNum]))
         for i in range(4):
             quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
