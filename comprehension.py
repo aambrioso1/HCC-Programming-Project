1f3d4fad4c4c5179197c02f1857fbc7540349979
Alex Ambrioso <aambrioso1@gmail.com>
2020-02-14 21:50:48 -0500

    Add files via upload

--- comprehension.py
+++ comprehension.py
@@ -11,34 +11,46 @@ comprehension.
 
 Here are the forms of comprehension in Python suggested by the article:
 
-new_list = [expression for member in iterable]
-new_list = [expression for member in iterable (if conditional)]
-new_list = [expression (if conditional) for member in iterable]
-
-At the end, I generate a set and a dictionary using comprehension.
+(3) new_list = [expression for member in iterable]
+(4) new_list = [expression for member in iterable (if conditional)]
+(5) new_list = [expression (if conditional) for member in iterable]
+(6) At the end, I generate a set and a dictionary using comprehension.
 
 """
 # We will generate lists using this string.
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-# (1) Construct a list using a loop
+# (1) Construct a list using a loop.
 list1 = []
 for letter in alphabet:
     list1.append(letter)
 
-# (2) Construct using the map function 
+# (2) Construct using the map function. 
 list2 = list(map(lambda i: i, alphabet))
 print('map =', list2)
 
+# (3) Construct a list using comprehension.
 list3 = [letter for letter in alphabet]
 
 print('list1 = ', list1)
 print('list1 = ', list2)
 print('list2 = ', list3)
 
-vowels = {i for i in alphabet+alphabet if i in 'aeiou'}
-print(vowels)
+# Examples using conditionals along with comprehension.
+# (4) new_list = [expression for member in iterable (if conditional)]
+vowels = {i for i in alphabet if i in 'aeiou'}
+print('The vowels are', vowels)
 
+# (4) new_list = [expression for member in iterable (if conditional)]
+
+consonants =  {i for i in alphabet if i not in 'aeiou'}
+print('The consonants are', consonants)
+
+# (5) new_list = [expression (if conditional) for member in iterable]
+delete_vowels = [letter if letter not in 'aeiou' else '*' for letter in alphabet]
+print('Letters with vowels marked', delete_vowels)
+
+# (6) Create a dictionary using comprehension.
 dict1 = {i: alphabet[i] for i in range(26)}
 print('The last letter of the alphabet is', dict1[25])
 
