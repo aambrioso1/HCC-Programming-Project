f64e9ab0f03a224ebe9effa8f62d2e034512a376
aambrioso <aambrioso1@gmail.com>
2020-11-13 13:44:36 -0500

    Modify add_files.

--- add_files.py
+++ add_files.py
@@ -1,5 +1,5 @@
 """
-A script for make mulitple copies of a file.
+A script for make multiple copies of a file.
 
 This program is based on Chapter 10: Organizing files of 
 Al Swiegart's nice book Automate the Boring Stuff 
@@ -8,7 +8,8 @@ There is lots more information about organizing files in the book.
 """
 
 
-# import standard libraries
+# import standard libraries for working with files (shutil), different operating systems (os).
+# and file paths (pathlib).
 import shutil, os
 from pathlib import Path
 
@@ -17,9 +18,9 @@ print(f'The current working directory is {p}')
 
 base_file = 'Item30.py' # The name of the file to be copied.
 
-# We use a loop to copy the base file over and over from Item{a}.py to Item{b}.py
+# We use a loop to copy the base file over and over again from Item{a}.py to Item{b}.py
 a = 31
 b = 90
-for i in range(a, b+1):
+for i in range(a, b+1): # Note that the range will run from the integer a to the integer b.
 	new_file = f'Item{i}.py'
 	shutil.copy(p/base_file, p/new_file)
