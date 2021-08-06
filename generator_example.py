66d457bb78f412c87561ed51cc7b9d9488fd8717
aambrioso1 <aambrioso1@gmail.com>
2021-08-06 16:08:21 -0400

    refactor generator_example

--- generator_example.py
+++ generator_example.py
@@ -1,24 +1,28 @@
 # generator_example.py
-"""An example of a generator.   We create an iterator that generates the index of all the occurrence of a word in a text.
-Rather than create a list to save the indices, we create a generator that will iterate through all of the indices.
+"""An example of a generator.   We create an iterator that generates 
+the index of all the occurrence of a word in a text.  Rather than 
+create a list to save the indices, we create a generator that will 
+iterate through all of the indices.
 There are several advantages:
 (1)  Less memory is used.
-(2)  The code is easier to read than it would be used a list and appended indices to it. [EP}
+(2)  The code is easier to read than it would be if we used a list and 
+appended indices to it.
 
-To do
-Generalize to any word
-Create a book retrieval shell command
-Remove any language from EP
+Interesting addition
+Create shell command htat would allow the user to provide the url.
 """
 import requests
 
-url = "https://www.gutenberg.org/files/1342/1342-0.txt" # text file from GP of Pride and Prejudice
+# text file from GP of Pride and Prejudice
+url = "https://www.gutenberg.org/files/1342/1342-0.txt" 
 
 response = requests.get(url)
 if response.status_code == 404:
 	raise SystemExit(f'HTTP response status: {res.status_code}') # Exit with message if URL is bad
 
-book = response.text[2440:] # book text starts at index 2440
+START = 2440 # start of the texts of the book
+
+book = response.text[START:] # book text starts at index 2440
 my_word = 'the '
 
 # define the generator just like a function except we use yield instead of return.
@@ -36,4 +40,7 @@ print(next(it))
 
 indices = list(index_of_word_iterator(book, my_word)) # We use the iterator to generate a list
 print(indices[:20]) # print the first 20 indices.
-print(f'The text at book[155:159] is "{book[155:159]}".') # We check the text at index 155
+
+start_index = indices[0]
+end_index = indices[0] + len(my_word)
+print(f'The text at {start_index} is "{book[start_index:end_index]}".') # We check the text at index 155
